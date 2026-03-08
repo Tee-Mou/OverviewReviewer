0a9a8aa73c9a15fcad74403a1079451c97cb094c
Tee-Mou <tennysonmorris@outlook.com>
2026-03-08 01:04:10 +0000

    Added querying LLM using Ollama API. Will be able to better test when using GPU.

--- scripts/overviewReviewer.js
+++ scripts/overviewReviewer.js
@@ -22,6 +22,24 @@ function reviewPopup(confirmPopup, cancelPopup) {
     }
 }
 
+async function query_llm(query) {
+    let url = "http://localhost:11434/api/chat";
+    let prompt = "Please verify the validity of the following overview. Give an accuracy score out of 100.";
+    prompt = prompt.concat(query);
+    let params = {
+        "model": "llama2",
+        "messages": [
+            {"role": "user", "content": prompt}
+        ]
+    };
+    const options = {
+        method: "POST",
+        body: JSON.stringify(params)
+    };
+    let response = await (await fetch(url, options)).text();
+    return response;
+}
+
 function openReview() {
     return new Promise((resolve, reject) => {
         reviewPopup(resolve, reject);
@@ -31,21 +49,20 @@ function openReview() {
 function scrapeOverview(selector) {
     return new Promise(resolve => {
         var innerText = selector.innerText;
-        var html = selector;
-        resolve(html, innerText);
+        resolve(innerText);
     })
 }
 
 async function checkPage() {
-    var markdown = null;
     waitForElement(`div[aria-label="Show more AI Overview"]`, async (showMore) => {
         await showMore.click();
     });
     await delay(500);
     waitForElement(`div[jsname][data-rl]`, async (overview) => {
-        scrapeOverview(overview).then(async (html, innerText) => {
-            console.log(html);
+        scrapeOverview(overview).then(async (innerText) => {
             console.log(innerText);
+            let llm_response = await query_llm(innerText);
+            console.log(llm_response)
         });
     });
 
